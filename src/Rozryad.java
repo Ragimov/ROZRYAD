f8b9dbfe42a047700b49ca37a870c7aeae494e5b
Elmar <relmar@mail.ua>
2016-04-27 19:10:18 +0300

    Initial commit

---
+++ src/Rozryad.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Acer on 27.04.2016.
+ */
+public class Rozryad {
+
+
+        public static void main(String[] args) {
+
+            int n = Integer.parseInt(args[0]);
+            int Roz = 1;
+            if(n <10)
+                if(Roz ==0)
+                {
+                    System.out.println("Rozryad 1");
+                    return;
+                }
+
+            System.out.println("Rozryad=" + n);
+        }
+
+}
